6e08da2000470b1fe28582b813d63be49571a925
Jeongeun <baro980906@gmail.com>
2021-08-10 01:13:27 +0900

    paser added

--- core/solver.py
+++ core/solver.py
@@ -3,28 +3,38 @@ import torch.optim as optim
 from core.MLN.model import MixtureLogitNetwork_cnn
 from core.MLN.loss import mace_loss
 from core.MLN.eval import func_eval,test_eval
+from core.utils import print_n_txt
 
 class solver():
-    def __init__(self):
-        self.EPOCH = 10
-        self.mode_name = 'mln'
-        self.data_size = (-1,1,28,28)
-        self.device = 'cuda'
-        self.init_model()
+    def __init__(self,args,device):
+        self.EPOCH = args.epoch
+        self.mode_name = args.mode
+        if args.dataset == 'mnist':
+            self.data_size = (-1,1,28,28)
+            self.labels=10
+        elif args.dataset == 'cifar10':
+            self.data_size = (-1,3,32,32)
+            self.labels=10
+        self.device = device
+        self.load_model(args)
         self.optimizer = optim.Adam(self.model.parameters(),lr=1e-3,weight_decay=1e-4,eps=1e-8)
-        self.labels=10
-    def init_model(self):
+        self.lambda1 = args.lambda1
+        self.lambda2 = args.lambda2
+        self.method = args.query_method
+        self.query_size = args.query_size
+
+    def load_model(self,args):
         if self.mode_name == 'mln':
             self.model = MixtureLogitNetwork_cnn(name='mln',x_dim=[1,28,28],k_size=3,c_dims=[32,64,128],p_sizes=[2,2,2],
-                            h_dims=[128,64],y_dim=10,USE_BN=False,k=10,
-                            sig_min=1.0,sig_max=10, 
+                            h_dims=[128,64],y_dim=self.labels,USE_BN=False,k=args.k,
+                            sig_min=args.sig_min,sig_max=args.sig_max, 
                             mu_min=-1,mu_max=+1,SHARE_SIG=True).to(self.device)
     def init_param(self):
         self.model.init_param()
 
-    def train_image(self,train_iter):
+    def train_classification(self,train_iter,test_iter,f):
         #scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[30,60,90,120,150,180], gamma=args.lr_rate)
-        # scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, gamma=0.2, step_size=5)
+        scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, gamma=0.2, step_size=20)
         for epoch in range(self.EPOCH):
             loss_sum = 0.0
             #time.sleep(1)
@@ -34,28 +44,46 @@ class solver():
                 target = torch.eye(self.labels)[batch_out].to(self.device)
                 target=target.to(self.device)
                 loss_out = mace_loss(pi,mu,sigma,target) # 'mace_avg','epis_avg','alea_avg'
-                loss = loss_out['mace_avg'] - loss_out['epis_avg'] + loss_out['alea_avg']
+                loss = loss_out['mace_avg'] - self.lambda1 * loss_out['epis_avg'] + self.lambda2 * loss_out['alea_avg']
                 #print(loss)
                 self.optimizer.zero_grad() # reset gradient
                 loss.backward() # back-propagation
                 self.optimizer.step() # optimizer update
                 # Track losses
                 loss_sum += loss
-            #scheduler.step()
+            scheduler.step()
             loss_avg = loss_sum/len(train_iter)
-            print(loss_avg.item())
+            test_out = test_eval(self.model,test_iter,self.data_size,'cuda')
+            train_out = test_eval(self.model,train_iter,self.data_size,'cuda')
+
+            strTemp = ("epoch: [%d / %d] loss: [%.3f] train_accr:[%.4f] test_accr: [%.4f]"
+                        %(epoch,self.EPOCH,loss_avg,train_out['val_accr'],test_out['val_accr']))
+            print_n_txt(_f=f,_chars=strTemp)
+
+            strTemp =  ("[Train] mace_avg:[%.4f] epis avg:[%.3f] alea avg:[%.3f] pi_entropy avg: [%.3f]"%
+                (loss_out['mace_avg'],loss_out['epis_avg'],loss_out['alea_avg'],loss_out['pi_entropy_avg']))
+            print_n_txt(_f=f,_chars=strTemp)
+
+            strTemp =  ("[Test] epis avg:[%.3f] alea avg:[%.3f] pi_entropy avg: [%.3f]"%
+                    (test_out['epis'],test_out['alea'],test_out['pi_entropy']))
+            print_n_txt(_f=f,_chars=strTemp)
+            
     
-    def query_data(self,infer_iter, method='epistemic',query_number=20):
+    def query_data(self,infer_iter):
         out = func_eval(self.model,infer_iter,self.data_size,'cuda')
-        if method == 'epistemic':
+        if self.method == 'epistemic':
             out = out['epis_']
-        elif method == 'aleatoric':
+        elif self.method == 'aleatoric':
             out = out['alea_']
+        elif self.method == 'maxsoftmax':
+            out = out['maxsoftmax_']
+        elif self.method == 'entropy':
+            out = out['entropy_']
+        elif self.method == 'pi_entropy':
+            out = out['pi_entropy_']
+        else:
+            raise NotImplementedError()
         out  = torch.FloatTensor(out)
-        _, max_idx = torch.topk(out,query_number,0)
+        _, max_idx = torch.topk(out,self.query_size,0)
         max_idx = max_idx.type(torch.LongTensor)
         return max_idx
-    
-    def test_acc(self,test_iter):
-        out = test_eval(self.model,test_iter,self.data_size,'cuda')
-        print("ACC: {}".format(out['val_accr']))
--- main.py
+++ main.py
@@ -1,20 +1,76 @@
 from core.pool import AL_pool
 from core.solver import solver
 from core.data import total_dataset
+from core.utils import print_n_txt
 import torch
+import random
+import numpy as np
 
-p = AL_pool()
-test_dataset = total_dataset(train=False)
-test_iter = torch.utils.data.DataLoader(test_dataset, batch_size=128, 
+import argparse
+import os
+import logging
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--mode', type=str,default='mln',help='[base ,mln, mdn]')
+parser.add_argument('--dataset', type=str,default='mnist',help='dataset_name')
+parser.add_argument('--root', type=str,default='./dataset',help='root directory of the dataset')
+parser.add_argument('--id', type=int,default=1,help='id')
+
+parser.add_argument('--query_step', type=int,default=20,help='query step')
+parser.add_argument('--query_size', type=int,default=20,help='query size')
+parser.add_argument('--init_dataset', type=int,default=100,help='number of initial data')
+parser.add_argument('--query_method', type=str,default='epistemic',help='query method')
+parser.add_argument('--epoch', type=int,default=10,help='epoch')
+
+parser.add_argument('--lr', type=float,default=1e-3,help='learning rate')
+parser.add_argument('--batch_size', type=int,default=128,help='batch size')
+parser.add_argument('--wd', type=float,default=1e-4,help='weight decay')
+
+parser.add_argument('--lambda1', type=float,default=1,help='hyper parameter for mln loss')
+parser.add_argument('--lambda2', type=float,default=1,help='hyper parameter for mln loss')
+
+parser.add_argument('--k', type=int,default=10,help='number of mixtures')
+parser.add_argument('--sig_min', type=float,default=1,help='sig min')
+parser.add_argument('--sig_max', type=float,default=10,help='sig max')
+
+args = parser.parse_args()
+
+SEED = 1234
+EPOCH = args.epoch
+
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+device='cuda'
+
+p = AL_pool(root=args.root,dataset_name=args.dataset,num_init=args.init_dataset)
+test_dataset = total_dataset(root = args.root, dataset_name=args.dataset, train=False)
+test_iter = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, 
                         shuffle=False)
-AL_solver = solver()
+
+AL_solver = solver(args,device=device)
 AL_solver.init_param()
 
 train_iter,query_iter = p.subset_dataset(torch.zeros(size=(0,1),dtype=torch.int64).squeeze(1))
 
-for i in range(20):
-    AL_solver.train_image(train_iter)
-    AL_solver.test_acc(test_iter)
+DIR = './res/{}_{}_{}/{}/'.format(args.mode,args.dataset,args.query_method,args.id)
+try:
+    os.mkdir('./res/{}_{}_{}'.format(args.mode,args.dataset,args.query_method))
+except:
+        pass
+for i in range(args.query_step):
+    try:
+        os.mkdir(DIR)
+    except:
+        pass
+    txtName = (DIR+'{}_log.txt'.format(i))
+    f = open(txtName,'w') # Open txt file
+    print_n_txt(_f=f,_chars='Text name: '+txtName)
+    print_n_txt(_f=f,_chars=str(args))
+    AL_solver.train_classification(train_iter,test_iter,f)
     id = AL_solver.query_data(query_iter)
     new = p.unlabled_idx[id]
     train_iter,query_iter = p.subset_dataset(new)
